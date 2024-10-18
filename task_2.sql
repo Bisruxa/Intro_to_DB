578421845d27bbee8560069c00c1887f8bd628e3
bisruxa <imbisru@gmail.com>
2024-10-18 09:57:33 -0900

    table created

--- task_2.sql
+++ task_2.sql
@@ -21,7 +21,7 @@ CREATE TABLE orders(
  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
   order_date DATE
 )
-CREATE TABLE order_details(
+CREATE TABLE Order_Details(
   orderdetail INT PRIMARY KEY,
    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
  FOREIGN KEY (book_id) REFERENCES Books(book_id),
